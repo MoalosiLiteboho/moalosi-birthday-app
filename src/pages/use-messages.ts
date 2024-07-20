f346e911b3dab6f2d464d6af3a7159691bfd0547
liteboho moalosi <litebohojamesmoalosi@gmail.com>
2024-07-20 15:18:53 +0200

    add changes

--- src/pages/use-messages.ts
+++ src/pages/use-messages.ts
@@ -1,36 +1,15 @@
 import {useEffect, useState} from "react";
+import {getAllMessages} from "@/pages/message-service.ts";
 import {MessageType} from "@/pages/message-card.tsx";
 
 export const useMessages = () => {
     const [messages, setMessages] = useState<MessageType[]>([]);
 
     const fetchMessages = () => {
-        setMessages([
-            {
-                "name": "Payment received",
-                "message": "Magic UI",
-                "time": "15m ago",
-                "icon": "💸"
-            },
-            {
-                "name": "User signed up",
-                "message": "Magic UI",
-                "time": "10m ago",
-                "icon": "👤"
-            },
-            {
-                "name": "New message",
-                "message": "Magic UI",
-                "time": "5m ago",
-                "icon": "💬"
-            },
-            {
-                "name": "New event",
-                "message": "Magic UI",
-                "time": "2m ago",
-                "icon": "🗞️"
-            }
-        ])
+        getAllMessages().then(res => {
+            console.log(res.data)
+            setMessages(res.data);
+        });
     }
 
     useEffect(() => {
