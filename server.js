102465df24919a95736cc3c6e07878ebce594e61
liteboho moalosi <litebohojamesmoalosi@gmail.com>
2024-07-20 17:13:53 +0200

    add changes

--- server.js
+++ server.js
@@ -4,7 +4,12 @@ const router = jsonServer.router('db.json'); // Your JSON data file
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
+server.use(
+    jsonServer.rewriter({
+        "/api/*": "/$1",
+    })
+);
 server.use(router);
-server.listen(process.env.PORT || 3000, () => {
-    console.log('JSON Server is running');
+server.listen(3000, () => {
+    console.log("JSON Server is running");
 });
